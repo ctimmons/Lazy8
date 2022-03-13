02b916e446ccd5dd10d0368c532ab4547de23a0a
Chris R. Timmons <chris@crtimmonsinc.com>
2022-03-13 13:56:02 -0500

    Added GuardedHashSet class and test.

---
+++ Lazy8.Core.Tests/HashSet.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Lazy8.Core.Tests
+{
+  [TestFixture]
+  public class HashSetTests
+  {
+    [Test]
+    public void GuardedHashSetTest()
+    {
+      /* Don't bother checking for a null predicate.
+         Let the C# compiler's null checking option detect that
+         condition at compile time.  In other words, the project file contains
+         <Nullable>enable</Nullable> */
+      //GuardedHashSet<String> foo = new(null);
+
+      GuardedHashSet<String> foo = new(s => !String.IsNullOrWhiteSpace(s));
+      Assert.That(foo.Add(""), Is.False);
+      Assert.That(foo.Add(" "), Is.False);
+      Assert.That(foo.Add("x"), Is.True);
+
+      Assert.That(foo.Count == 1, Is.True);
+      Assert.That(foo.Contains(""), Is.False);
+      Assert.That(foo.Contains(" "), Is.False);
+      Assert.That(foo.Contains("x"), Is.True);
+    }
+  }
+}
